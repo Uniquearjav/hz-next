b49dfd34f65fb6c028a8b8e475606e3efd82cd44
Arjav Choudhary <92720814+Uniquearjav@users.noreply.github.com>
2024-02-05 17:40:31 +0000

    y

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,15 @@ export const metadata = {
   description: `Horizaura: Enhancing homes with beautifully crafted elegance. Explore our stunning selection of sofas, home decor, and captivating resin plates. Experience the blend of artistic craftsmanship and comfort in every aspect. Welcome to a world where creativity meets refined style.`
 }
 
-function page() {
+async function getProducts() {
+  const response = await fetch('https://dummyjson.com/products');
+  const products = await response.json();
+  console.log(products.products[0]);
+  return products.products;
+}
+
+export async function page() {
+  const products = await getProducts();
   return (
     <main>
         <section id="Hero">
@@ -26,6 +34,31 @@ function page() {
   </div>
 </div>
 </section>
+<section id="ProductHero">
+      <h2 className="text-5xl text-center p-10">Browse The Range</h2>
+      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        {products.map((product) => (
+          <div key={product.id} className="products w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+              <Image src={`/media/products/HZ${product.id}.jpg`} alt="Product" width={288} height={320} className="h-80 w-72 object-cover rounded-t-xl" />
+              <div className="px-4 py-3 w-72">
+                <span className="text-gray-400 mr-3 uppercase text-xs">HORIZAURA</span>
+                <p className="text-lg font-bold text-black truncate block capitalize">{ product.title}</p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">&#8377; { product.price}</p>
+                  <div className="ml-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
+    </section>
     </main>
   )
 }
