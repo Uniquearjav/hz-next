db96032961439c21b3baba0931a0bf6d4e05b102
Arjav Choudhary <92720814+Uniquearjav@users.noreply.github.com>
2024-02-06 10:58:37 +0000

    lo

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 }
 
 async function getProducts() {
-  const response = await fetch('/api/products.json');
+  const response = await fetch('https://www.horizaura.tech/api/products.json');
   const products = await response.json();
   console.log(products);
   return products;
